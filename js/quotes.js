f8588e568681bb48f0fc736eb926236d83ba37e2
Hassan Murtaza <hassan@Hassans-MacBook-Pro.local>
2018-08-04 00:41:37 -0500

    Finishing up project and implemented extra credit

---
+++ js/quotes.js
@@ -0,0 +1,48 @@
+// Create the array of quote objects and name it quotes
+
+quotes = [
+    {
+        quote : 'Monopoly is the condition of every successful business.',
+        source : 'Peter Thiel',
+        citation : "Zero to One",
+        year : '2014',
+        category : 'Business'
+
+    },
+
+    {
+        quote : 'The secret of getting ahead is getting started.',
+        source : 'Mark Twain',
+        citation : '',
+        year : '1997',
+        category : 'Inspirational'
+    },
+
+    // source for citation : https://www.businessinsider.com/amazon-ceo-jeff-bezos-quotes-2015-7#on-innovation-i-think-frugality-drives-innovation-just-like-other-constraints-do-one-of-the-only-ways-to-get-out-of-a-tight-box-is-to-invent-your-way-out-2
+    {
+        quote : 'I think frugality drives innovation, just like other constraints do. One of the only ways to get out of a tight box is to invent your way out.',
+        source : 'Jeff Bezos',
+        citation : '',
+        year : '2008',
+        category : 'Innovation'
+    },
+
+    
+    //source at: http://www.keepinspiring.me/famous-quotes/#ixzz5NBIqhruI',
+
+    {
+        quote : 'Those who dare to fail greatly can ever achieve greatly.',
+        source : 'John F. Kennedy',
+        citation : 'Day of Affirmation Address',
+        year : '1966',
+        category : 'Motivational'
+    },
+
+    {
+        quote : 'Well done is better than well said.',
+        source : 'Benjamin Franklin',
+        citation : '',
+        year : '18th Century',
+        category : 'Motivational'
+    },
+];
--- js/script.js
+++ js/script.js
@@ -1,17 +1,68 @@
 // FSJS - Random Quote Generator
 
-// Create the array of quote objects and name it quotes
 
 
+// Create the getRandomQuote function and name it getRandomQuote
 
-// Create the getRandomQuuote function and name it getRandomQuote
+function getRandomQuote() {
+    if (quotes === undefined) {             //Making sure the quotes array exists
+        throw "Quotes is not defined!";
+    }
+    if (quotes.length <= 0) {                   //Error checking to see if there are quotes inside the array
+        throw "There seems to be no quotes!";
+    }
 
+    var randomNum = Math.floor(Math.random() * quotes.length);  //getting a random number in between 0 to length-1
 
+    return quotes[randomNum];
+}
 
 // Create the printQuote funtion and name it printQuote
+function printQuote() {
+    var randomQuote = getRandomQuote();
 
+    if (randomQuote === undefined) {        // Making sure the quote returned actually exists
+        throw "This quote does not exist!";
+    }
+
+    //Creating string to display on html page
+    var quoteDisplay = `
+    <p class="quote"> ${randomQuote.quote} </p>
+    <p class="source"> ${randomQuote.source}
+      <span class="citation">${randomQuote.citation}</span>
+      <span class="year"> ${randomQuote.year} </span>
+    </p>`;
+
+
+    changeBackgroundColor();    // Function will randomly change the background color
+
+    //Putting the quote inside the html page
+    document.getElementById('quote-box').innerHTML = quoteDisplay;
+}
+
+
+// Generates random rgb value
+function getRandomColor() {
+    var rgbVals = [];
+    var randomColor;
+
+    for (var i = 0; i < 3; i++) {
+        randomColor = Math.floor(Math.random() * 256);  //Gets a value in between 0-255    
+        rgbVals.push(randomColor);
+    }
+    return rgbVals;
+}
+
+//Uses random rgb value and applies it to the background
+function changeBackgroundColor() {
+    var rgbVals = getRandomColor();
+    document.body.style.backgroundColor = 'rgb(' + rgbVals.join(',') + ')';
+}
 
 
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+//Sets the interval so the function is ran every 30 seconds whether user clicks the button or not
+setInterval(printQuote,30000);
